f0869abbdd06f975646c5a2c1304248deb4d8086
Dongin Kim <donginkim88@gmail.com>
2019-01-06 23:27:12 +0900

    created: neural_network_00*

---
+++ deeplearning_101/neural_network_00_exe.py
@@ -0,0 +1,15 @@
+from neural_network_00 import *
+
+if __name__ == "__main__":
+    X = np.array([[0,0,1],
+                  [0,1,1],
+                  [1,0,1],
+                  [1,1,1]])
+    y = np.array([[0],[1],[1],[0]])
+    nn = NeuralNetwork(X,y)
+
+    for i in range(1500):
+        nn.feedforward()
+        nn.backprop()
+
+    print(nn.output)
