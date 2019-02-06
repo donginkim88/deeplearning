d34160ce0887fd94c6dd773c886bdd5c0a99ee54
dongin <donginkim88@gmail.com>
2019-02-06 22:00:00 +0900

    added: w1 and w2

---
+++ Tutorial/py_for_everyone_04_function.py
@@ -0,0 +1,33 @@
+import turtle as t
+import random
+
+score = 0
+playing = False
+
+#enemy
+te = t.Turtle()
+te.shape("turtle")
+te.color("red")
+te.speed(0)
+te.up()
+te.goto(0, 200)
+
+#prey
+ts = t.Turtle()
+ts.shape("circle")
+ts.color("green")
+ts.speed(0)
+ts.up()
+ts.goto(0, -200)
+
+def turn_right():
+    t.setheading(0)
+
+def turn_right():
+    t.setheading(0) 
+
+def turn_right():
+    t.setheading(0)
+
+def turn_right():
+    t.setheading(0)
